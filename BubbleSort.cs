3d72e1a02f87d1d4b35540aa33a149edf35bc79e
Cam <cam_chambers@outlook.com>
2021-08-17 08:46:10 -0400

    Cleanup

--- BubbleSort.cs
+++ BubbleSort.cs
@@ -1,17 +1,14 @@
-
 class BubbleSort
 {
     public int[] sort(int[] items)
     {
         int length = items.Length;
-
         int temp = items[0];
-
         for (int i = 0; i < length; i++)
         {
             for (int j = i + 1; j < length; j++)
             {
-                // Determine if the current item in the array is greater than
+                // Determine if the item at the current index the array is greater than
                 // the next item in the array
                 if (items[i] > items[j])
                 {
